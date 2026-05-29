f4905ad8015b1abdfa8ea88c0315000b52a61206
ShurGith <jujogonve@gmail.com>
2026-05-29 09:43:35 +0200

    feat: Seed initial book data

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -18,8 +18,18 @@ class DatabaseSeeder extends Seeder
         // User::factory(10)->create();
 
         User::factory()->create([
+            'name' => 'JuanJota',
+            'email' => 'juanjota@example.com',
+            'password' => 'password',
+        ]);
+        User::factory()->create([
             'name' => 'Test User',
             'email' => 'test@example.com',
+            'password' => 'password',
+        ]);
+
+        $this->call([
+            BookSeeder::class,
         ]);
     }
 }
